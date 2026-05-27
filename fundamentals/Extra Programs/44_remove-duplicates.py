ff6b632278b538119d565d1c37b255aa9336ac0d
Nikhil R. Parmar <nikhilparmarofc@gmail.com>
2026-05-27 15:10:52 +0530

    Create 44_remove-duplicates.py

---
+++ fundamentals/Extra Programs/44_remove-duplicates.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 2, 3, 4, 4, 5]
+
+unique_numbers = []
+
+for num in numbers:
+
+    if num not in unique_numbers:
+        unique_numbers.append(num)
+
+print("Original List:", numbers)
+print("Without Duplicates:", unique_numbers)
