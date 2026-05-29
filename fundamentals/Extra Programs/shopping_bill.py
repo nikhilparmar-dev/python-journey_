dc4293783d59258a25a7091b0fc650eb383a173c
Nikhil R. Parmar <nikhilparmarofc@gmail.com>
2026-05-29 15:21:40 +0530

    Create shopping_bill.py

---
+++ fundamentals/Extra Programs/shopping_bill.py
@@ -0,0 +1,7 @@
+price1 = float(input("Enter first item price: "))
+price2 = float(input("Enter second item price: "))
+price3 = float(input("Enter third item price: "))
+
+total = price1 + price2 + price3
+
+print("Total Bill =", total)
